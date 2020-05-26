8d6cf5a11981a7ea4d26ce2aef2cbfdae4fc6377
Daniel Andrew <danwandrew@gmail.com>
2020-05-26 14:40:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(string)
-  if string == string.upcase
-    "NO, NOT SINCE 1938!"
-  elsif string == 'I LOVE YOU GRANDMA!'
+  if string == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
+  elsif string == string.upcase
+    "NO, NOT SINCE 1938!"
   else 'HUH?! SPEAK UP, SONNY!'
   end
 end
